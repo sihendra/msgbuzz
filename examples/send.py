83f7c8ea74dab9a98b580990fbe39f249e43f57e
Hendra Setiawan <hendra.setiawan@shortlyst.ai>
2020-03-04 09:49:21 +0700

    Fix publish method in example/send.py

--- examples/send.py
+++ examples/send.py
@@ -1,10 +1,11 @@
+from msgbuzz import Message
 from msgbuzz.rabbitmq import RabbitMqMessageBus
 
 if __name__ == '__main__':
     msg_bus = RabbitMqMessageBus()
 
     for i in range(2):
-        msg_bus.publish('profile.new', f'Profile New {i + 1} !!', headers={"header1": "value"})
+        msg_bus.publish('profile.new', Message({"header-1": "value-1"}, f'Message {i + 1} !!'))
 
     for i in range(2):
-        msg_bus.publish('profile.complete', f'Profile Complete {i + 1} !!', headers={"header1": "value"})
+        msg_bus.publish('profile.complete', Message({"header-1": "value-2"}, f'Message {i + 1} !!'))
